255135b9f30855208d2e5e456b423cbedce824c4
Muhammad Ardie <ardiemuhammad3@gmail.com>
2020-03-03 18:12:18 +0700

    change port setting

--- app/config/index.js
+++ app/config/index.js
@@ -2,7 +2,7 @@ var config = module.exports
 var PRODUCTION = process.env.NODE_ENV === 'production'
 
 config.express = {
-  port: process.env.EXPRESS_PORT || 3000,
+  port: process.env.PORT || 3000,
   ip: 'localhost'
 }
 
